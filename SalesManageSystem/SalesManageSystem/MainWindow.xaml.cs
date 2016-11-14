305c2f86932cbcc311d229c986175b0cf9c94c25
mmx <751762390@qq.com>
2016-11-14 21:00:49 +0800

    modified MainWindow.xaml

--- SalesManageSystem/SalesManageSystem/MainWindow.xaml.cs
+++ SalesManageSystem/SalesManageSystem/MainWindow.xaml.cs
@@ -24,5 +24,23 @@ namespace SalesManageSystem
         {
             InitializeComponent();
         }
+
+        private void ListBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBox listBox = sender as ListBox;
+            
+            switch (listBox.SelectedIndex) {
+                case 0:
+                    break;
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                default:
+                    break; 
+            }
+        }
     }
 }
